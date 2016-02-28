3af66c2843eca7973e46427afccb1fddf7845118
Mac Workman <workman.mac@gmail.com>
2016-02-28 04:42:44 -0500

    real-time serial monitoring implemented in visualization A

---
+++ matlab/visualization_A.m
@@ -0,0 +1,106 @@
+%% Visualization for Prototype Arrays A0/A1/A2
+%
+% This matlab script renders the activation level of the prototype A arrays
+% visually and in real-time. The data is collected and plotted until the
+% user terminates the collection of the data on the Raspberry Pi. Note that
+% the visualization is pseudo-binary, and can say nothing about the actual
+% capacitance being seen at the electrodes, only whether or not the
+% electrode has been activated.
+%
+% Author:   Mac Workman
+% Case ID:  maw157
+% Class:    EECS 399
+% Project:  Flexible Sensor Array
+
+%% Initialize the serial port object
+%
+% The value of the 'port' variable should be changed depending on which
+% operating system you are using and what kind of chip/driver combination
+% your serial cable uses. Examples:
+%
+% Mac OS/Linux:         port = '/dev/tty...' or '/dev/cu...'
+% Windows:              port = 'COM1'
+%
+% Additionally, the serial object properties should be set to match the
+% properties used by the serial_write.py module on the Raspberry Pi. The
+% baud rate can be changed to one of the standard values [9600, 19200,
+% 38400, 57600, 115200], but the other settings are best left alone.
+
+port = '/dev/cu.SLAB_USBtoUART';
+baudrate = 9600;
+parity = 'none';
+stopbits = 1;
+bytesize = 8;
+timeout = 1;
+
+obj = serial(port,...
+             'BaudRate',baudrate,...
+             'Parity',parity,...
+             'StopBits',1,...
+             'DataBits',bytesize,...
+             'Timeout',timeout);
+
+fopen(obj);
+
+%% Set up the figure window
+% This is done ahead of time in order to make plotting the data in
+% real-time with as little delay as possible.
+
+figure_handle = figure('NumberTitle','off',...
+                       'Name','Visualization A',...
+                       'Visible','off');
+
+axes_handle = axes('Parent',figure_handle,'YDir','reverse');
+axis equal;
+title('Visualization A','FontSize',15,'FontWeight','bold')
+hold on;
+
+% Initialize empty visualization matrix
+M = zeros(37,22);
+no_handle = 1;
+
+%% Begin data collection and visualization
+
+while BytesAvailable > 0
+    % Collect data from serial port and separate electrode streams
+    data = fscanf(port);
+    vars = textscan(data,'%f%f%f%f%f%f%f%f%f%f%f%f','delimiter',',');
+    [cap1, cap2, cap3, cap4, cap5, cap6,...
+        cap7, cap8, cap9, cap10, cap11, cap12] = deal(vars{:});
+
+    % Draw electrodes and update activation status
+    M(2:9,2:7) = cap10;
+    M(2:9,9:14) = cap11;
+    M(2:9,16:21) = cap12;
+    
+    M(11:18,2:7) = cap7;
+    M(11:18,9:14) = cap8;
+    M(11:18,16:21) = cap9;
+    
+    M(20:27,2:7) = cap4;
+    M(20:27,9:14) = cap5;
+    M(20:27,16:21) = cap6;
+    
+    M(29:36,2:7) = cap3;
+    M(29:36,9:14) = cap2;
+    M(29:36,16:21) = cap1;
+    
+    % Plot data - if we don't have a handle for the contour object yet, get
+    % one so that we can set the 'ZData' property to update the
+    % visualization more quickly in future loop-throughs
+    if no_handle
+        [~,contour_handle] = contourf(M,10);
+        set(figure_handle,'Visible','on')
+        no_handle = 0;
+    else
+        set(contour_handle,'ZData',M)
+        drawnow
+    end
+    
+    pause(0.01)
+end
+
+%% Clean up serial object
+fclose(obj);
+delete(obj);
+clear obj;
